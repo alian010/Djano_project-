07aa9d70a42d92573f2dd2d4304309b3f3bdb9b0
Shakhawat Hossen <shakhawat016192@gmail.com>
2025-08-31 14:44:13 +0600

    social app commit

---
+++ social/social_book/core/views.py
@@ -0,0 +1,174 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.contrib import messages
+from .models import Post, Profile,likePost,FollowersCount
+# Create your views here.
+
+#@login_required(login_url='singin')
+def index(request):
+    user_object = User.objects.get(username = request.user.username)
+    user_profile = Profile.objects.get(user = user_object)
+    
+    posts = Post.objects.all()
+    return render(request,'index.html' ,{'user_profile':user_profile,'posts':posts})
+
+
+
+@login_required(login_url ="signin")
+def upload(request):
+    if request.method == "POST":
+        user= request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+        
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+        return redirect('/')
+    else:
+        return redirect('/')
+    
+@login_required(login_url='singin')
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+    post = Post.objects.get(id=post_id)
+    
+    like_filter = likePost.objects.filter(post_id=post_id, username=username).first()
+    
+    if like_filter == None:
+        new_like = likePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes + 1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes  = post.no_of_likes - 1
+        post.save()
+        return redirect('/')
+
+
+def follow(request):
+    if request.method == "POST":
+        follower = request.POST['follower']
+        user = request.POST['user']
+        if FollowersCount .objects.filter(follower=follower, user=user).first():
+            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
+            delete_follower.delete()
+            return redirect('/profile/'+user)
+        else:
+            new_follower = FollowersCount.objects.create(follower=follower, user=user)
+            new_follower.save()
+            return redirect('/profile/'+user)
+    
+    else:
+        return redirect('/')
+    
+   
+@login_required(login_url='singin')
+def profile(request, pk):
+    user_object = User.objects.get(username=pk)
+    user_profile = Profile.objects.get(user=user_object)
+    user_posts = Post.objects.filter(user=pk)
+    user_post_length = len(user_posts)
+
+    context = {
+        'user_object': user_object,
+        'user_profile': user_profile,
+        'user_posts': user_posts,
+        'user_post_length': user_post_length,
+        
+    }
+
+    return render(request, 'profile.html', context)
+
+
+
+
+
+@login_required(login_url='singin')
+def setting(request):
+    user_profile = Profile.objects.get(user= request.user)
+    if request.method == "POST":
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+            
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+            
+            user_profile.profileimg = image   
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        return redirect('setting') 
+    #if request.method == "POST":
+    return render(request,'setting.html',{'user_profile':user_profile})
+
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'Email taken')
+                return redirect('signup')
+            elif User.objects.filter(username= username).exists(): 
+                messages.info(request,'username taken')
+                return redirect('signup')
+            else:
+                user =User.objects.create_user(username = username ,email=email,password=password)
+                user.save()
+                
+                user_login = authenticate(username = username, password = password)
+                login(request,user_login)
+                
+                #
+                user_model = User.objects.get(username =username) 
+                new_profile = Profile.objects.create(user=user_model,id_user = user_model.id)
+                new_profile.save()
+                messages.success(request,'create account successful !')
+                return redirect('setting')
+        else:
+            messages.error(request,'password not matching ')
+            return redirect ('signup')
+            
+      
+    return render(request,'signup.html')
+
+
+
+def signin(request):
+    if request.method =="POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(username = username, password=password )
+       
+        if user is not None: 
+            messages.success(request,'log in success')
+            login(request,user)
+            return redirect('/')
+        else:
+            messages.error(request,'invalid')
+            return redirect('signin')
+    return render(request,'signin.html')
+
+@login_required(login_url='singin')
+def Logout(request):
+    logout(request)
+    return redirect('signin') 
